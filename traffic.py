538919e2a58b20b12450821dd7a16c99f224788e
darinastav <dashon@ukr.net>
2025-09-08 16:12:08 +0300

    test

---
+++ traffic.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+
+url_2023 = (r"C:\Users\user\PycharmProjects\data-analytics\input data\202306\202306\Route_2306.csv\Route_2306.csv")
+file = pd.read_csv(url_2023)
