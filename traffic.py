c3bb58525cdd76d6149de8c8304d82bbd5ff62a1
darinastav <dashon@ukr.net>
2025-09-08 16:51:40 +0300

    test

--- traffic.py
+++ traffic.py
@@ -1,5 +1,14 @@
 import pandas as pd
 import numpy as np
 
-url_2023 = (r"C:\Users\user\PycharmProjects\data-analytics\input data\202306\202306\Route_2306.csv\Route_2306.csv")
+#reading a file
+url_2023 = (r"C:\Users\user\PycharmProjects\data-analytics\input data\202306\202306\Flights_20230601_20230630.csv\Flights_202306.csv")
 file = pd.read_csv(url_2023)
+
+#cleaning
+file.info()
+x = file.isnull().sum()
+y = file.isnull().sum().sum()
+replace_null = file.fillna(np.nan)
+drop_null = file.dropna(how='any')
+
