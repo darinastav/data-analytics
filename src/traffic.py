a3a4dde666f2024fc640b030bb21bc18c31fadc6
darinastav <dashon@ukr.net>
2025-09-09 23:45:12 +0300

    cleaning and EDA sections updated

--- src/traffic.py
+++ src/traffic.py
@@ -15,9 +15,24 @@ with zipfile.ZipFile(zip_file_name, 'r') as zip_file:
         file = pd.read_csv(csv_file)
 
 #cleaning
-file.info()
-x = file.isnull().sum()
-y = file.isnull().sum().sum()
-replace_null = file.fillna(np.nan)
-drop_null = file.dropna(how='any')
+#file.info()
+file.isnull().sum()
+file.duplicated().sum()
+convert_date = file['ACTUAL OFF BLOCK TIME'].str.split(' ', n=1, expand=True)[0]
+file['Date'] = convert_date
+drop_june = file[file['Date'] == '01-07-2023']
+drop_may = file[file['Date'] == '31-05-2023']
+x = file.drop(drop_june.index, inplace=True)
+y = file.drop(drop_may.index, inplace=True)
 
+#EDA(Exploratory Data Analysis)
+total_flights = file.shape[0]
+print(f'The total amount of flights is: {total_flights}')
+
+daily_flights = file.groupby('Date').size().reset_index(name='Flights') #counts flights per day
+average_flights_per_day = daily_flights['Flights'].mean()
+print(f'The average number flights per day is: {average_flights_per_day:.0f}')
+
+busiest_day = daily_flights.loc[daily_flights['Flights'].idxmax(), 'Date']
+max_flights = daily_flights.loc[daily_flights['Flights'].idxmax(), 'Flights']
+print(f'The busiest day in June 2023 is: {busiest_day} with {max_flights} flights')
