9fd388f0404bf3eea792a521f9fe149ac819c3d3
darinastav <dashon@ukr.net>
2025-09-10 22:28:06 +0300

    cleaned and updated

--- src/traffic.py
+++ src/traffic.py
@@ -19,35 +19,32 @@ with zipfile.ZipFile(zip_file_name, 'r') as zip_file:
         file_2022 = pd.read_csv(csv_file)
 
 #cleaning
-#file.info()
+file_2023.info()
 file_2023.isnull().sum()
 file_2023.duplicated().sum()
-#convert_date = file_2023['ACTUAL OFF BLOCK TIME'].str.split(' ', n=1, expand=True)[0]
-#file_2023['Date'] = convert_date
-#file_2023['Date'] = pd.to_datetime(file_2023['ACTUAL OFF BLOCK TIME'])
-file_2023['Date'] = pd.to_datetime(file_2023['ACTUAL OFF BLOCK TIME'], format='%d-%m-%Y')
-#file_2023['Date'] = pd.to_datetime(file_2023['Date'] )  # ensure datetime type
-#file_2023['Date'] = pd.to_datetime(file_2023['ACTUAL OFF BLOCK TIME'], dayfirst=True, errors='coerce')
-#print(file_2023['Date'])
-drop_june = file_2023[file_2023['Date'] == '01-07-2023']
-drop_may = file_2023[file_2023['Date'] == '31-05-2023']
-x = file_2023.drop(drop_june.index, inplace=True)
-y = file_2023.drop(drop_may.index, inplace=True)
+file_2023['Date'] = pd.to_datetime(file_2023['ACTUAL OFF BLOCK TIME'], dayfirst=True, errors='coerce')
+
+file_2023 = file_2023[~file_2023['Date'].dt.date.isin(
+    [pd.to_datetime("2023-07-01").date(),
+     pd.to_datetime("2023-05-31").date()])
+]
 
 #EDA(Exploratory Data Analysis)
 total_flights_2023 = file_2023.shape[0]
 total_flights_2022 = file_2022.shape[0]
-pct_change = ((total_flights_2023 - total_flights_2022)/total_flights_2023)*100
+pct_change = ((total_flights_2023 - total_flights_2022)/total_flights_2022)*100
 print(f'The total amount of flights is: {total_flights_2023}')
-print(f'The percentage change in 2023: {pct_change:.0f}%')
+print(f'The percentage change in 2023 comparing to 2022: {pct_change:.0f}%')
 
-daily_flights = file_2023.groupby('Date').size().reset_index(name='Flights') #counts flights per day
+daily_flights = file_2023.groupby(pd.Grouper(key='Date', freq='D')).size().reset_index(name='Flights')
 average_flights_per_day = daily_flights['Flights'].mean()
 print(f'The average number flights per day is: {average_flights_per_day:.0f}')
 
 busiest_day = daily_flights.loc[daily_flights['Flights'].idxmax(), 'Date']
 max_flights = daily_flights.loc[daily_flights['Flights'].idxmax(), 'Flights']
-print(f'The busiest day in June 2023 is: {busiest_day} with {max_flights} flights')
+print(f'The busiest day in June 2023 is: {busiest_day.date()} with {max_flights} flights')
 
 weekly_flights = file_2023.groupby(pd.Grouper(key='Date', freq='W-MON')).size().reset_index(name='Flights')
-print(weekly_flights)
+max_week = weekly_flights.loc[weekly_flights['Flights'].idxmax(), 'Date']
+max_week_flights = weekly_flights.loc[weekly_flights['Flights'].idxmax(), 'Flights']
+print(f'The busiest week was {max_week.date()} with {max_week_flights} flights')
