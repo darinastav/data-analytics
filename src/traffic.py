079618cf81df267fc71868464454a37026457362
darinastav <dashon@ukr.net>
2025-09-14 21:49:42 +0300

    total ATFCM delays calculated

--- src/traffic.py
+++ src/traffic.py
@@ -19,17 +19,24 @@ with zipfile.ZipFile(zip_file_name, 'r') as zip_file:
         file_2022 = pd.read_csv(csv_file)
 
 #cleaning
-file_2023.info()
-file_2023.isnull().sum()
+#file_2023.info()
+x = file_2023.isnull().sum()
+y = file_2022.isnull().sum()
 file_2023.duplicated().sum()
-file_2023['Date'] = pd.to_datetime(file_2023['ACTUAL OFF BLOCK TIME'], dayfirst=True, errors='coerce')
-
+file_2023['Date'] = pd.to_datetime(file_2023['ACTUAL OFF BLOCK TIME'], dayfirst=True, errors='coerce') #convert actual off block time from str to date
+file_2023['Date Filed'] = pd.to_datetime(file_2023['FILED OFF BLOCK TIME'], dayfirst=True, errors='coerce') #convert filed off block time from str to date
 file_2023 = file_2023[~file_2023['Date'].dt.date.isin(
     [pd.to_datetime("2023-07-01").date(),
-     pd.to_datetime("2023-05-31").date()])
-]
+     pd.to_datetime("2023-05-31").date()])] #to remove May and July from a loop 2023
+
+file_2022['Date'] = pd.to_datetime(file_2022['ACTUAL OFF BLOCK TIME'], dayfirst=True, errors='coerce') #do the same as above but for 2022 year
+file_2022['Date Filed'] = pd.to_datetime(file_2022['FILED OFF BLOCK TIME'], dayfirst=True, errors='coerce')
+file_2022 = file_2022[~file_2022['Date'].dt.date.isin(
+    [pd.to_datetime("2022-07-01").date(),
+     pd.to_datetime("2022-05-31").date()])]
 
 #EDA(Exploratory Data Analysis)
+#traffic
 total_flights_2023 = file_2023.shape[0]
 total_flights_2022 = file_2022.shape[0]
 pct_change = ((total_flights_2023 - total_flights_2022)/total_flights_2022)*100
@@ -48,3 +55,32 @@ weekly_flights = file_2023.groupby(pd.Grouper(key='Date', freq='W-MON')).size().
 max_week = weekly_flights.loc[weekly_flights['Flights'].idxmax(), 'Date']
 max_week_flights = weekly_flights.loc[weekly_flights['Flights'].idxmax(), 'Flights']
 print(f'The busiest week was {max_week.date()} with {max_week_flights} flights')
+
+#ATFCM delays
+file_2023['Actual_minutes'] = file_2023['Date'].dt.hour * 60 + file_2023['Date'].dt.minute #extract hours and minutes from datetime
+file_2023['Filed_minutes']  = file_2023['Date Filed'].dt.hour * 60 + file_2023['Date Filed'].dt.minute
+file_2023['Delay'] = file_2023['Actual_minutes'] - file_2023['Filed_minutes']
+
+file_2023['Delay'] = np.where(file_2023['Delay'] < 0,
+                              file_2023['Delay'] + 24*60,
+                              file_2023['Delay']) #to cal when arrival is after the midnight
+total_delay_2023 = file_2023['Delay'].sum() #cal total delay in minutes
+average_delay_2023 = file_2023['Delay'].mean()
+
+print(f'The total delay in 2023 is: {total_delay_2023} minutes')
+print(f'The average delay is: {average_delay_2023:.3f} minutes')
+
+file_2022['Actual_minutes'] = file_2022['Date'].dt.hour * 60 + file_2022['Date'].dt.minute #extract hours and minutes from datetime
+file_2022['Filed_minutes']  = file_2022['Date Filed'].dt.hour * 60 + file_2022['Date Filed'].dt.minute
+file_2022['Delay'] = file_2022['Actual_minutes'] - file_2022['Filed_minutes']
+
+file_2022['Delay'] = np.where(file_2022['Delay'] < 0,
+                              file_2022['Delay'] + 24*60,
+                              file_2022['Delay']) #to cal when arrival is after the midnight
+total_delay_2022 = file_2022['Delay'].sum() #cal total delay in minutes
+average_delay_2022 = file_2022['Delay'].mean()
+print(f'The total delay in 2022 is: {total_delay_2022} minutes')
+print(f'The average delay is: {average_delay_2022:.3f} minutes')
+
+pct_delay_change = ((average_delay_2023 - average_delay_2022)/average_delay_2022)*100
+print(f'The percentage change of delays in 2023 comparing to 2022 is: {pct_delay_change:.0f}% up')
